24191aceb910b00e89f6ced40d67bdf30b5aa533
Hazel Windle <hwindle83@googlemail.com>
2023-03-18 14:48:38 +0000

    Tidyed up navbar and search

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import NavBar from '../components/Navbar';
 import { SearchComp } from '../components/SearchComp';
-import { MovieCard } from '../components/MovieCard';
+//import { MovieCard } from '../components/MovieCard';
 
 export default function Search() {
   return (
@@ -12,10 +12,13 @@ export default function Search() {
         <div className='wrapper mt-4'>
           <SearchComp />
           <Row md={3} xs={1} lg={4} className='g-4 mt-3'>
-            {movieSearchResults?.map((item) => (
+            {/* {movieSearchResults?.map((item) => (
               <Col key={item.id}>
                 <MovieCard movie={item} />
               </Col>
+            ))} */}
+            {[0, 1, 2, 3, 4].map((item) => (
+              <Col key={item}>{item}</Col>
             ))}
           </Row>
         </div>
