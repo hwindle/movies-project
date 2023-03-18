85efa4b2df5e22ef2056c7bac2f7be5bdedc703c
Hazel Windle <hwindle83@googlemail.com>
2023-03-18 15:02:48 +0000

    Tidyed up the home page so there were no errors

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import NavBar from '../components/Navbar';
-import { SearchComp } from '../components/SearchComp';
+//import { SearchComp } from '../components/SearchComp';
 //import { MovieCard } from '../components/MovieCard';
 
 export default function Search() {
