4972c572f4072e70a3372e5a1b685f86f48e6892
Hazel Windle <hwindle83@googlemail.com>
2023-03-18 15:04:59 +0000

    Got the template to run

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -10,7 +10,7 @@ export default function Search() {
       <NavBar />
       <Container className='mt-4'>
         <div className='wrapper mt-4'>
-          <SearchComp />
+          {/* <SearchComp /> */}
           <Row md={3} xs={1} lg={4} className='g-4 mt-3'>
             {/* {movieSearchResults?.map((item) => (
               <Col key={item.id}>
