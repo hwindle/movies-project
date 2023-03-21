f71f7946df2af20e1a0bf82f54ed53d2f19f7a0e
Divya Jain <djain.4444@gmail.com>
2023-03-21 13:22:06 +0530

    resolved value null issue, added css to modal, changes to modal to display correct data

--- src/components/InfoModal.jsx
+++ src/components/InfoModal.jsx
@@ -1,107 +1,67 @@
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import Modal from "react-bootstrap/Modal";
+import React from "react";
+import "./InfoModal.css";
 
 function InfoModal(props) {
-  const [show, setShow] = useState(true);
-  const [movieInfo, setMovieInfo] = useState([]);
-  const [refreshModal, setRefresh] = useState(true);
-  const [validData, setValidData] = useState(false);
-
-  // useEffect for getting products
-
-  useEffect(() => {
-    //console.log('inside the useEffect');
-    setShow(props.show);
-
-    async function getInfo() {
-      try {
-        console.log('calling async api');
-
-        // const movieUrl = `${process.env.REACT_APP_BE_LOCAL}/moviedetails?id=${props.movieid}`;
-        // const newData = await axios.get(movieUrl);
-
-        // console.log(' here is the data   ', newData);
-
-        setRefresh(false);
-        setMovieInfo(props.data);
-        console.log(movieInfo);
-        setValidData(true);
-        // response.status(200).send(digiData);
-      } catch (error) {
-        console.log(error);
-        console.log('error in acquiring movie info');
-
-        //response.status(500).send("error in request for images");
-      }
-      //setCallApi(false);
-    }
-
-    if (refreshModal) {
-      getInfo();
-    }
-  }, [props.movieid, refreshModal, props.show, movieInfo, props.data]);
-
-  // handler for close button icon in header
-
-  const handleClose = () => setShow(false);
-
-  if (validData) {
+  if (props.show) {
     let formattedTime = 0;
-    if (parseInt(movieInfo.runtime) > 60) {
-      formattedTime = Math.floor(parseInt(movieInfo.runtime) / 60) + ' hrs and ';
-      formattedTime += (parseInt(movieInfo.runtime) % 60) + ' mins';
+    if (parseInt(props.data.runtime) > 60) {
+      formattedTime =
+        Math.floor(parseInt(props.data.runtime) / 60) + " hrs and ";
+      formattedTime += (parseInt(props.data.runtime) % 60) + " mins";
     } else {
-      formattedTime = movieInfo.runtime + ' mins';
+      formattedTime = props.data.runtime + " mins";
     }
 
     return (
-      <Modal show={show} onHide={handleClose}>
+      <Modal show={props.show} onHide={props.handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>{movieInfo.title}</Modal.Title>
+          <Modal.Title style={{ color: "white", fontWeight: "bold" }}>
+            {props.data.title}
+          </Modal.Title>
         </Modal.Header>
-        <Modal.Body>
+        <Modal.Body style={{ color: "#afafaf", fontWeight: "bold" }}>
           <div>
             <p>
-              <span style={{ fontWeight: 'bold' }}>Overview: </span>
-              {movieInfo.overview}
+              <span>Overview: </span>
+              {props.data.overview}
             </p>
             <p>
-              <span style={{ fontWeight: 'bold' }}>Release Date: </span>
-              {movieInfo.release_date}
+              <span style={{ fontWeight: "bold" }}>Release Date: </span>
+              {props.data.release_date}
             </p>
             <p>
-              <span style={{ fontWeight: 'bold' }}>Original Language: </span>
-              {movieInfo.original_language}
+              <span style={{ fontWeight: "bold" }}>Original Language: </span>
+              {props.data.original_language}
             </p>
             <p>
-              <span style={{ fontWeight: 'bold' }}>Imdb ID: </span>IMDB id: 
-              {movieInfo.imdb_id}
+              <span style={{ fontWeight: "bold" }}>Imdb ID: </span>
+              {props.data.imdb_id}
             </p>
             <p>
-              <span style={{ fontWeight: 'bold' }}>Budget: </span>$
-              {movieInfo.budget}
+              <span style={{ fontWeight: "bold" }}>Budget: </span>$
+              {props.data.budget}
             </p>
             <p>
-              <span style={{ fontWeight: 'bold' }}>Revenue: </span>$
-              {movieInfo.revenue}
+              <span style={{ fontWeight: "bold" }}>Revenue: </span>$
+              {props.data.revenue}
             </p>
             <p>
-              <span style={{ fontWeight: 'bold' }}>Run time: </span>
+              <span style={{ fontWeight: "bold" }}>Run time: </span>
               {formattedTime}
             </p>
             <p>
-              <span style={{ fontWeight: 'bold' }}>Popularity: </span>Popularity: 
-              {movieInfo.popularity}
+              <span style={{ fontWeight: "bold" }}>Popularity: </span>
+
+              {props.data.popularity}
             </p>
             <p>
-              <span style={{ fontWeight: 'bold' }}>Vote Count: </span>Vote count: 
-              {movieInfo.vote_count}
+              <span style={{ fontWeight: "bold" }}>Vote Count: </span>
+              {props.data.vote_count}
             </p>
             <p>
-              <span style={{ fontWeight: 'bold' }}>Vote Average: </span>Vote avg: 
-              {movieInfo.vote_average}
+              <span style={{ fontWeight: "bold" }}>Vote Average: </span>
+              {props.data.vote_average}
             </p>
           </div>
         </Modal.Body>
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import Card from 'react-bootstrap/Card';
-import Stack from 'react-bootstrap/Stack';
-import { Tooltip } from 'react-tooltip';
-import { Star, InfoSquare, Trash3 } from 'react-bootstrap-icons';
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Stack from "react-bootstrap/Stack";
+import { Tooltip } from "react-tooltip";
+import { Star, InfoSquare, Trash3 } from "react-bootstrap-icons";
 
 function MovieCard(props) {
   const movie = props.movie;
   const { poster_path, title, release_date, overview } = movie;
 
   return (
-    <Card style={{ width: '18rem' }}>
+    <Card style={{ width: "18rem" }}>
       <Card.Img
         variant="top"
         src={`https://image.tmdb.org/t/p/original/${poster_path}`}
@@ -20,12 +20,14 @@ function MovieCard(props) {
           Release date: {release_date}
         </Card.Subtitle>
         <Card.Text>{overview}</Card.Text>
-        <Stack direction="horizontal" gap={3} style={{ margin: '5px' }}>
+        <Stack direction="horizontal" gap={3} style={{ margin: "5px" }}>
           <div>
-            {props.buttonvariant !== '0' && (
+            {props.buttonvariant !== "0" && (
               <button
-                style={{ marginRight: '3px', border: '0px' }}
-                onClick={props.infohandler}
+                style={{ marginRight: "3px", border: "0px" }}
+                onClick={() => {
+                  props.infohandler(props.idx);
+                }}
                 //idx={props.idx}
                 data-tooltip-id="viewTip"
                 data-tooltip-content="Movie Info"
@@ -33,42 +35,42 @@ function MovieCard(props) {
                 <InfoSquare
                   color="gray"
                   size={32}
-                  //onClick={props.infohandler}
-                  idx={props.idx}
+                  // idx={props.idx}
                 />
                 <Tooltip id="viewTip" />
               </button>
             )}
           </div>
-          <div style={{ marginLeft: 'auto' }}>
-            {props.buttonvariant === '1' && (
+          <div style={{ marginLeft: "auto" }}>
+            {props.buttonvariant === "1" && (
               <button
-                style={{ marginRight: '3px', border: '0px' }}
-                onClick={props.favhandler}
-                idx={props.idx}
+                style={{ marginRight: "3px", border: "0px" }}
+                onClick={() => {
+                  props.favhandler(props.idx);
+                }}
+                // idx={props.idx}
                 data-tooltip-id="favTip"
                 data-tooltip-content="Add to Favourites"
               >
                 <Star
-                  //onClick={props.favhandler}
-                  idx={props.idx}
+                  // idx={props.idx}
                   color="gray"
                   size={32}
                 />
                 <Tooltip id="favTip" />
               </button>
             )}
-            {props.buttonvariant === '2' && (
+            {props.buttonvariant === "2" && (
               <button
-                style={{ marginRight: '3px', border: '0px' }}
-                onClick={props.delhandler}
-                idx={props.idx}
+                style={{ marginRight: "3px", border: "0px" }}
+                onClick={() => props.delhandler(props.idx)}
+                // idx={props.idx}
                 data-tooltip-id="delTip"
                 data-tooltip-content="Delete Movie"
               >
                 <Trash3
                   //onClick={props.delhandler}
-                  idx={props.idx}
+                  // idx={props.idx}
                   color="gray"
                   size={32}
                 />
